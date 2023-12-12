2f0ac54e30768d3b12a213d16e73a1d62bca9dd2
samir <sc9018@ic.ac.uk>
2023-12-12 08:52:29 +0000

    skeleton class methods added

--- handling.py
+++ handling.py
@@ -30,6 +30,13 @@ class PaperHandling:
         """
         return (f'{self.__class__.__name__}('f'{self.paper_name!r})')
     
+    def text_parse(self, extracted_text:str):
+        """
+        helper method to parse extracted text 
+        :param extracted_text: the extracted text upon which to apply the parser
+        """
+        
+
     def unwrap_pdf(self):
         """
         method to unwrap pdf into data we want
@@ -37,6 +44,24 @@ class PaperHandling:
         reader = PdfReader(f"./papers/{self.paper_name}.pdf")
         page = reader.pages[0]
         print(page.extract_text())
+
+    def unwrap_images(self):
+        """
+        method to unwrap images
+        """
+
+    def reference_web(self):
+        """
+        method to implement the reference web
+        """
+
+
+class LLM_Agent:
+    """
+    
+    """
+
+
 
 paper = PaperHandling("UJPR-4-1-RW1")
 paper.unwrap_pdf()
