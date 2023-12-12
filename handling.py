15ed2859ca27210d32fcdcb1745d3b7b59a50a53
samir <sc9018@ic.ac.uk>
2023-12-12 11:45:27 +0000

    working query

--- handling.py
+++ handling.py
@@ -1,7 +1,7 @@
 import re
 from PyPDF2 import PdfReader
 import os
-import openai
+from openai import OpenAI
 
 
 class PaperHandling:
@@ -50,6 +50,7 @@ class PaperHandling:
         self.content = str(self.content.encode("utf-8", errors="replace"))
         self.content = self.content.replace("\\n","\n")
         print("the type of bb is ", self.content)
+        return self.content
         
         
     def unwrap_images(self):
@@ -71,26 +72,60 @@ class LLM_Agent:
     def __init__(self,temp):
         self.temp = temp
 
+    def __repr__(self):
+        return (f'{self.__class__.__name__}')
+
     @staticmethod
     def read_api_key(file_path='keys.txt'):
+        """
+        -- API KEY --
+        """
         with open(file_path, 'r') as file:
             api_key = file.read().strip()
         return api_key
 
     def openai_verify(self):
+        """
+        openAI verify 
+        """
         key = self.read_api_key()
-        openai.api_key = os.getenv(key) or key
-        print(openai.Engine.list())
+        self.client = OpenAI(api_key=key)
+        OpenAI.api_key = os.getenv(key) or key
+
+    def query(self,prompt):
+        """
+        method to query llm
+        :param prompt: the prompt
+        :param context: the context in question
+        """
+
+        completion = self.client.chat.completions.create(
+        model = 'gpt-3.5-turbo-16k',
+        messages = [
+        {'role': 'user', 'content': prompt}],
+        temperature = 1  )
+        print(completion.choices[0].message.content)
+
+        # result = openai.Completion.create(
+        #     engine = "gpt-4",
+        #     prompt = prompt,
+        #     max_tokens=2000
+        # )
+        # print(result['choices'][0]['text'].strip())
 
         
 
 
 
 paper = PaperHandling("UJPR-4-1-RW1")
-paper.unwrap_pdf()
+context = paper.unwrap_pdf()
 
 llm = LLM_Agent(10)
 llm.openai_verify()
+prompt_pt = ":::\nbased on the previous text tell me one interesting fact"
+print(context + prompt_pt)
+print("the output is ,\n")
+llm.query(context + prompt_pt)
 
 
 
